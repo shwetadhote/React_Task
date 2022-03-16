e0bd06dc95f112c8af8883a95037fda66f9e2918
Shweta <96809628+shwetadhote@users.noreply.github.com>
2022-03-16 21:42:53 +0530

    Add files via upload

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,43 +1,45 @@
-import React, { useState } from 'react'
-import styles from './style.modules.css'
+import React from 'react'
+
+
+const Home = (props) => {
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        console.log("submitted")
+    }
+    return (
+        <>
+        <div>
+            <h1>Hello{props.name}
+            </h1>
+        </div>
+
+        <form action="" onClick={HandleSubmit}>
+            <button >Submit</button>
+        </form>
+        </>
+    )
+}
+/*const HandleClick = () => {
+    console.log("Clicked")
+}
+
+const HandleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit")
+}
 
 const Home = () => {
+  return (
+      <>    
+      <div>Onclick Event</div>
+      <button onClick={HandleClick}>Click</button>
+      <form onSubmit={HandleSubmit}>
+      <button type="submit">Submit</button>
+      </form>
+    </>
+  )
+};*/
 
-  let newTime = new Date().toLocaleTimeString();
-  const [ctime,setCtime] = useState(newTime);
 
-  const UpdateTime = () => {
-    newTime = new Date().toLocaleTimeString();
-    setCtime(newTime);
-  }
-  
-  //setInterval(UpdateTime, 1000)
-  
-    return(
-      <>
-      <h1>{ctime}</h1>
-      <button onClick={UpdateTime}>Get Time</button>
-      </>
-    )
-
-  }
 
 export default Home;
-/*
-const Home = () => {
-
-    const [count, setCount] = useState(0);
-
-const IncNum = () => {
-  setCount(count + 1);
-};
-  return (
-
-    <>
-    <h1>{count}</h1>
-    <button onClick={IncNum}> Click Me </button>
-    </>
-  );
-}*/
-
-
